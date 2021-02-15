e174717a4487fcf3a79ca28f4bb50c7a6e12184d
KARTHIK ANIL <karthik030700@gmail.com>
2021-02-15 13:29:46 +0530

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -6,19 +6,24 @@ $(document).ready(function(){
         request.onload = function(){
             var data=JSON.parse(request.responseText);
             load(data);
+            
         };
+        
         request.send();
     })
-    $("#del").click(function(){
-        
-        $("#p_"+i).hide();
-         i++;
-         
-        
+    $(document).on('click',".del", function(){
+       var x=this.id;
+       
+        $("#"+x).remove();
+        $(this).remove();
+  
     });
+        
+    
     $("#btn_1").click(function(){
         
         $("p").hide();
+        $(".del").remove();
         
     });
     
@@ -27,11 +32,11 @@ function load(data){
    
     for(i=0;i<data.length;i++){
         
-        $('#content').append("<p id='p_"+i+"'><span><b>User Id : </b>" + data[i].userId +"</span><br> <span><b>Id : </b>" +data[i].id+ "</span><br><span><b>Title : </b>"+data[i].title+" </span><br><span><b>Body : </b>"+data[i].body+"</span><br></p>" )
-        
+        $('#content').append("<p id='p_"+i+"'><span><b>User Id : </b>" + data[i].userId +"</span><br> <span><b>Id : </b>" +data[i].id+ "</span><br><span><b>Title : </b>"+data[i].title+" </span><br><span><b>Body : </b>"+data[i].body+"</span><br></p> " )
+        $('#content').append("<button id='p_"+i+"' class='del'>Delete</button>");
         
     }
-    $('p').show();
+    
     
 }
 
